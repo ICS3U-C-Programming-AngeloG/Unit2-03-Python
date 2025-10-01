9172173288764003726f968ac8e98e0a78f8edd9
Angelo Garcia <angelo.garcia@stu.ocsb.ca>
2025-10-01 17:47:10 +0000

    added pizza_cost.py, and constants.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created by: Angelo Garcia
+# Created on: September 30th, 2025
+# This program contains constants.
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.50
